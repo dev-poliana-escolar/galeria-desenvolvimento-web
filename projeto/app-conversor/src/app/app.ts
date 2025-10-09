810fea0580b41b4cb5193d90ddcac48ccb600097
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2025-10-09 09:51:06 -0300

    feat: aplicação: conversor de temperatura

--- projeto/app-conversor/src/app/app.ts
+++ projeto/app-conversor/src/app/app.ts
@@ -1,21 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   temp_celsius = 0;
-  temp_fahreinheit= 32;
-  
+  temp_fahrenheit= 32;
+  resultado : boolean = false;
 
   conversor(){
     // Celsius * 9/5) + 32
-    this.temp_fahreinheit = (this.temp_celsius * (9/5)) + 32
-     
+    this.temp_fahrenheit = (this.temp_celsius * (9/5)) + 32
+    this.resultado = true;
   }
+ 
 
 }
